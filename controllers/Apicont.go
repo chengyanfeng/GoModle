ba4c72e21dcc366f6a6e4a88e8f1aa6b567624d7
yanfeng <chengyanfengchengyanfeng@genyuanlian.com>
2018-07-20 15:16:29 +0800

    添加mongod 说明 查询

--- controllers/Apicont.go
+++ controllers/Apicont.go
@@ -2,9 +2,9 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
-	."testthree/models"
+	."GoModle/models"
 	"fmt"
-	"testthree/util"
+	"GoModle/util"
 )
 type TestController struct {
 	beego.Controller
@@ -17,7 +17,16 @@ func (this *TestController) Getstudent(){
 	fmt.Print(stduenttree)
 	//DB.Model(stduenttree).Related(&credit, "Id")
 	//返回json 格式的字符串
+	queryp:=util.P{}
+	queryp["cname"]="aaaa"
+	//添加
 	util.D(util.Cheng).Add(stduenttree)
+	//查询所有的cname为aaaa 的
+	//db库下的test文档里的数据
+
+	a:=util.D("test").Find(queryp).All()
+	fmt.Print(a)
+	util.D(util.Cheng).Explain()
 	this.Ctx.WriteString(util.JsonEncode(stduenttree))
 
 
